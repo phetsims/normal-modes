48fdb3ecb89e62804bcb9468ff09f0eb67377d31
phet-dev <phetadmin@gmail.com>
2021-05-25 15:57:48 -0600

    update copyright dates from daily grunt work

--- js/common/view/NormalModesControlPanel.js
+++ js/common/view/NormalModesControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2021, University of Colorado Boulder
 
 /**
  * NormalModesControlPanel contains controls for both 1D and 2D views, including:
