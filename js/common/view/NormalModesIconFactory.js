2dbc6b4c57c44e2d89676f583437dd390427be72
pixelzoom <cmalley@pixelzoom.com>
2021-10-18 13:36:39 -0600

    fix the icon for 'One Dimension' screen, https://github.com/phetsims/qa/issues/725

--- js/common/view/NormalModesIconFactory.js
+++ js/common/view/NormalModesIconFactory.js
@@ -1,57 +1,53 @@
 // Copyright 2020, University of Colorado Boulder
 
 /**
- * Factory that creates the icons for the One and Two Dimensions Screens.
- * Currently unfinished.
+ * NormalModesIconFactory creates icons used in this simulation.
  *
  * @author Franco Barpp Gomes (UTFPR)
  * @author Thiago de Mendonça Mildemberger (UTFPR)
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Vector2 from '../../../../dot/js/Vector2.js';
 import Screen from '../../../../joist/js/Screen.js';
 import ScreenIcon from '../../../../joist/js/ScreenIcon.js';
 import Shape from '../../../../kite/js/Shape.js';
 import merge from '../../../../phet-core/js/merge.js';
+import Node from '../../../../scenery/js/nodes/Node.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
 import normalModes from '../../normalModes.js';
 import NormalModesColors from '../NormalModesColors.js';
 
-// TODO - Fix
 const NormalModesIconFactory = {
 
+  /**
+   * Creates the icon for the 'One Dimension' screen.
+   * @returns {ScreenIcon}
+   * @public
+   */
   createOneDimensionScreenIcon() {
 
     const rectSize = Screen.MINIMUM_HOME_SCREEN_ICON_SIZE;
-    const iconNode = new Rectangle( 0, 0, rectSize.width, rectSize.height, {
-      children: []
+
+    // The spring is a horizontal line.
+    const springShape = new Shape().moveTo( 0, 0 ).lineTo( rectSize.width, 0 );
+    const springNode = new Path( springShape, {
+      stroke: NormalModesColors.SPRING_STROKE,
+      lineWidth: 15
     } );
 
-    const mass = new Rectangle( merge( {
-      boundsMethod: 'unstroked',
-      lineWidth: 16,
-      rectWidth: 80,
-      rectHeight: 80
+    // The mass is a square.
+    const massLength = 0.15 * rectSize.width;
+    const massNode = new Rectangle( merge( {
+      lineWidth: 15,
+      rectWidth: massLength,
+      rectHeight: massLength,
+      center: springNode.center
     }, NormalModesColors.MASS_COLORS ) );
 
-    const springShape = new Shape().moveTo( 0, 0 ).lineTo( 0, 1 );
-
-    const spring = new Path( springShape, {
-      preventFit: true,
-      boundsMethod: 'none',
-      pickable: false,
-      inputEnabled: false,
-      stroke: NormalModesColors.SPRING_STROKE,
-      lineWidth: 50
+    const iconNode = new Node( {
+      children: [ springNode, massNode ]
     } );
-
-    spring.scale( 11, 15 );
-    iconNode.addChild( spring );
-    iconNode.addChild( mass );
-
-    mass.center = iconNode.center;
-    spring.translation = new Vector2( iconNode.centerX, iconNode.centerY - 15 );
 
     return new ScreenIcon( iconNode, {
       maxIconWidthProportion: 1,
@@ -59,6 +55,11 @@ const NormalModesIconFactory = {
     } );
   },
 
+  /**
+   * Creates the icon for the 'Two Dimensions' screen.
+   * @returns {ScreenIcon}
+   * @public
+   */
   createTwoDimensionsScreenIcon() {
     const rectSize = Screen.MINIMUM_HOME_SCREEN_ICON_SIZE;
     const iconNode = new Rectangle( 0, 0, rectSize.width, rectSize.height, {
