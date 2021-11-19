a6880d9905735b64c8b6d3a1ddc346a61ad22bf9
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:02 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/normalModesStrings.ts
@@ -0,0 +1,36 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import normalModes from './normalModes.js';
+
+type StringsType = {
+  'normal-modes': {
+    'title': string
+  },
+  'screen': {
+    'oneDimension': string,
+    'twoDimensions': string
+  },
+  'initialPositions': string,
+  'zeroPositions': string,
+  'numberOfMasses': string,
+  'showSprings': string,
+  'showPhases': string,
+  'normalMode': string,
+  'amplitude': string,
+  'normalModeSpectrum': string,
+  'phase': string,
+  'frequency': string,
+  'frequencyRatioOmegaPattern': string,
+  'normalModeAmplitudes': string
+};
+
+const normalModesStrings = getStringModule( 'NORMAL_MODES' ) as StringsType;
+
+normalModes.register( 'normalModesStrings', normalModesStrings );
+
+export default normalModesStrings;
