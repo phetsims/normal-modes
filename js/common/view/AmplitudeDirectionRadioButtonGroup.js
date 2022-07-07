70f6ef2cab2a9cf913f1a959edb8f0c533f5b8f0
phet-dev <phetadmin@gmail.com>
2022-07-07 05:07:04 -0600

    update copyright dates from daily grunt work

--- js/common/view/AmplitudeDirectionRadioButtonGroup.js
+++ js/common/view/AmplitudeDirectionRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Radio button group used to select movement axis (1D) or normal mode amplitude selector axes (2D).
