8376cca6e35ccb94c35f4f43442fe434138d5194
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/normalModesStrings.ts
+++ js/normalModesStrings.ts
@@ -5,28 +5,44 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import normalModes from './normalModes.js';
 
 type StringsType = {
   'normal-modes': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'oneDimension': string;
+    'oneDimensionProperty': TReadOnlyProperty<string>;
     'twoDimensions': string;
+    'twoDimensionsProperty': TReadOnlyProperty<string>;
   };
   'initialPositions': string;
+  'initialPositionsProperty': TReadOnlyProperty<string>;
   'zeroPositions': string;
+  'zeroPositionsProperty': TReadOnlyProperty<string>;
   'numberOfMasses': string;
+  'numberOfMassesProperty': TReadOnlyProperty<string>;
   'showSprings': string;
+  'showSpringsProperty': TReadOnlyProperty<string>;
   'showPhases': string;
+  'showPhasesProperty': TReadOnlyProperty<string>;
   'normalMode': string;
+  'normalModeProperty': TReadOnlyProperty<string>;
   'amplitude': string;
+  'amplitudeProperty': TReadOnlyProperty<string>;
   'normalModeSpectrum': string;
+  'normalModeSpectrumProperty': TReadOnlyProperty<string>;
   'phase': string;
+  'phaseProperty': TReadOnlyProperty<string>;
   'frequency': string;
+  'frequencyProperty': TReadOnlyProperty<string>;
   'frequencyRatioOmegaPattern': string;
+  'frequencyRatioOmegaPatternProperty': TReadOnlyProperty<string>;
   'normalModeAmplitudes': string;
+  'normalModeAmplitudesProperty': TReadOnlyProperty<string>;
 };
 
 const normalModesStrings = getStringModule( 'NORMAL_MODES' ) as StringsType;
